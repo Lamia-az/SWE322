06da9302576f774ce27de262bc241d96e8659214
Lamia-az <78917527+Lamia-az@users.noreply.github.com>
2021-03-26 15:12:14 +0300

    Create PS3-dataSearching.php

---
+++ PS3-dataSearching.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <form action="searching.php">
+        <select name="search">
+            <option value="phone"> phone </option>
+            <option value = "mobile"> mobile </option>
+            <option value="name"> name </option>
+        </select>
+        <br>
+        <input type="text" name="search_word" value=""/>
+        <br><br>
+        <input type="submit" name="" value="Search"/>
+    </form>
+</body>
+</html>
